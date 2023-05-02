06fafbe858654a0fd0e64e1731a00d54557a0704
HabibaSabrina <habibasabrina92@gmail.com>
2023-05-03 00:45:26 +0600

    chef server route is created

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const chefs = require('./data/chef.json')
 
 app.get('/', (req,res) =>{
     res.send('server is running')
+})
+app.get('/chef', (req,res) =>{
+    res.send(chefs)
 })
 app.listen(port, () => {
     console.log(`server is running on port: ${port}`)
