985728633a612d5f9fcafcf4b4ee2ed595fe2d79
HabibaSabrina <habibasabrina92@gmail.com>
2023-05-03 00:48:13 +0600

    cors installed and  required in

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
-const chefs = require('./data/chef.json')
+const chefs = require('./data/chef.json');
 
+app.use(cors())
 app.get('/', (req,res) =>{
     res.send('server is running')
 })
